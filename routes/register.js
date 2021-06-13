4638b26b1d00cb3aba2237a0d0964e8cef862936
lollipas <draakonlohe123@gmail.com>
2021-06-13 20:43:50 +0300

    updated registration

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* GET login page. */
 router.get('/', function (req, res, next) {
-    res.render('register', { title: 'register' });
+    res.render('register', { title: 'Register' });
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body.username)
+    console.log(req.body.password)
+    console.log(req.body.email)
+})
 module.exports = router;
